12c38a1cb24d8dea4f841b3e962feb0dca570bdd
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-06 08:24:10 +0200

    add test list and target parameter to main, add template findIndexOf()

--- src/runner.java
+++ src/runner.java
@@ -6,6 +6,27 @@
 //
 //Используйте в качестве образца задачу 2 из классной работы.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class runner {
 
+  public static void main(String[] args) {
+    List<Integer> test = new ArrayList<>();
+    test.add(9);
+    test.add(7);
+    test.add(5);
+    test.add(3);
+    test.add(1);
+
+    int target = 1;
+
+    int index = findIndexOf(test, target);
+
+    System.out.println(index);
+  }
+
+  private static int findIndexOf(List<Integer> ListOfData, int target) {
+    return -1;
+  }
 }
