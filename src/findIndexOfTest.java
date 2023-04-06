829074b700e457f4da61c52eea61083784f07614
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-06 16:08:53 +0200

    add 3 unittests and clean main

---
+++ src/findIndexOfTest.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class findIndexOfTest {
+
+  @Test
+  public void expectedNormalConditions() {
+    //arrange
+    List<Integer> test = new ArrayList<>();
+    test.add(9);
+    test.add(7);
+    test.add(5);
+    test.add(3);
+    test.add(1);
+
+    int target1 = 1;
+    int target2 = 9;
+
+    //act
+    int index1 = runner.findIndexOf(test, target1);
+    int index2 = runner.findIndexOf(test, target2);
+
+    //assert
+    assertEquals(4, index1);
+    assertEquals(0, index2);
+  }
+
+  @Test
+  public void noTargetParameterInList() {
+    //arrange
+    List<Integer> test = new ArrayList<>();
+    test.add(9);
+    test.add(5);
+    test.add(1);
+
+    int target = 3;
+
+    //act
+    int index = runner.findIndexOf(test, target);
+
+    //assert
+    assertEquals(-1, index);
+  }
+
+  @Test
+  public void emptyList() {
+    //arrange
+    List<Integer> test = new ArrayList<>();
+
+    int target = 2;
+
+    //act
+    int index = runner.findIndexOf(test, target);
+
+    //assert
+    assertEquals(-1, index);
+  }
+
+}
--- src/runner.java
+++ src/runner.java
@@ -7,30 +7,14 @@
 //Используйте в качестве образца задачу 2 из классной работы.
 
 //Plan max:
-//create binary search
-//add method description
+//+create binary search
+//+add method description
 //create unit-tests
 //create separate method to split ArrayList and LinkedList
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class runner {
-
-  public static void main(String[] args) {
-    List<Integer> test = new ArrayList<>();
-    test.add(9);
-    test.add(7);
-    test.add(5);
-    test.add(3);
-    test.add(1);
-
-    int target = 1;
-
-    int index = findIndexOf(test, target);
-
-    System.out.println(index);
-  }
 
   /**
    * Binary method for find target element in a pre-sorted descending list of distinct numbers.
@@ -39,7 +23,7 @@ public class runner {
    * @return - index of the searched component in the list or
    * return -1 if the element is not on the list
    */
-  private static int findIndexOf(List<Integer> listOfData, int target) {
+  public static int findIndexOf(List<Integer> listOfData, int target) {
     int left = 0;
     int right = listOfData.size() - 1;
     while (left < right) {
